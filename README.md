c1634e2bbed657b15da979a565123bda7eba5644
b-kettle83 <kettben832@gmail.com>
2025-03-04 16:57:01 -0500

    Add Communication
    This commit adds comments and a proper README file to help with user comprehension and organization, and also fixes some validation

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # b_kettlewell_crushingbugs
-This is the repo for my Multimedia Authoring class' crushing bugs assignment, focusing on fixing bugs to understand the functionality of functions and event listeners as well as drag and drop javascript mechanics
+This is the repo for my Multimedia Authoring class' crushing bugs assignment, focusing on fixing bugs to understand the functionality of functions and event listeners as well as drag and drop javascript mechanics.
+
+## Installation
+To run this project locally, follow these steps:
+1. Clone the repository
+
+## Usage
+1. Drag and drop puzzle pieces onto the puzzle board
+2. Press the background buttons to change the puzzle
+
+## Features
+- **Drag-and-Drop Functionality** → Add and remove puzzle pieces to the board
+- **Interactive UI** → Interactive buttons and drag-and-drop to customizes.
+
+## Contributing
+Please follow these steps:
+1. Fork the repository.
+2. Create a feature branch:  git checkout -b my-new-feature
+3. Commit your changes: git commit -am 'Add some feature'
+4. Push to the branch: git push origin my-new-feature
+5. Submit a pull request
+
+## History
+Created on February 26, 2025
+
+## Credits
+Officially Published by: Ben Kettlewell
+
+## Licence
+MIT Lisence Year 2025
--- js/main.js
+++ js/main.js
@@ -3,58 +3,82 @@ console.log('JS file connected');
 let theButtons = document.querySelectorAll('#buttonHolder img'),
     puzzleBoard = document.querySelector('.puzzle-board'),
     puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
+    dropZones = document.querySelectorAll('.drop-zone'),
+    draggedPiece,
+
+    // Store the original left container of the puzzle pieces
     originalContainer = document.querySelector('.puzzle-pieces'),
+
+    // Variables for each of the locations of puzzle pieces
     tlPiece = document.querySelector('.puzzle-pieces #tl'),
     trPiece = document.querySelector('.puzzle-pieces #tr'),
     blPiece = document.querySelector('.puzzle-pieces #bl'),
     brPiece = document.querySelector('.puzzle-pieces #br'),
-    dropZones = document.querySelectorAll('.drop-zone'),
-    originalZone,
-    zoneTaken,
-    draggedPiece;
 
+    // Variable to store the original zone of a picked up piece
+    originalZone,
+
+    // Boolean for if a zone is taken or not
+    zoneTaken;
+
+    // On background change
     function changeBGimage() {
+        // Change puzzle board background
         puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
+
+        // Change each piece's image to whichever one corresponds with the id of the button pressed
         tlPiece.src = `images/topLeft${this.id}.jpg`;
         trPiece.src = `images/topRight${this.id}.jpg`;
         blPiece.src = `images/bottomLeft${this.id}.jpg`;
         brPiece.src = `images/bottomRight${this.id}.jpg`;
 
+        // Place each puzzle piece in its original container (the left box)
         puzzlePieces.forEach(piece => originalContainer.appendChild(piece));
 
+        // Make every drop zone no longer taken, making them available for placement
         dropZones.forEach(zone => zone.zoneTaken = false);
     }
 
+    // When a piece is picked up
     function handlesStartDrag() {
         console.log('Started Dragging this piece: ', this);
+
+        // Set the picked up piece to be the currently dragged piece
         draggedPiece = this;
 
+        // Store the zone the piece was originally in
         originalZone = this.parentElement;
     }
 
+    // While a piece is held over a drop zone, prevent default
     function handleDragOver(e) {
         e.preventDefault();
         console.log('Dragged Over');
     }
 
+    // When a piece is dropped
     function handleDrop(e) {
         
         e.preventDefault();
 
+        // If the zone the piece is dropped in is taken, don't drop
         if (this.zoneTaken === true) {
             console.log('Zone Taken');
         }
 
+        //If the zone is not taken, drop the piece and set the zone to taken
         else {
             this.appendChild(draggedPiece);
             console.log('dropped');
             this.zoneTaken = true;
 
+            // Once the piece is dropped, make the take status of its original container false so it can be used again
             originalZone.zoneTaken = false;
         }
         
     }
 
+    // Run each function on user interaction
     theButtons.forEach(button => button.addEventListener('click', changeBGimage));
 
     puzzlePieces.forEach(piece => piece.addEventListener('dragstart', handlesStartDrag));
